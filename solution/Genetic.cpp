819bbb3bd9393fd42a1f652136647ee73b281870
BLGALEX <lepehaleha@bk.ru>
2021-02-22 04:54:55 +1000

    Добавил много геттеров в класс InputData, началписать функцию для старта генетического алгоритма и функцию для случайного изменения поколения.

--- solution/Genetic.cpp
+++ solution/Genetic.cpp
@@ -5,6 +5,7 @@
 #include "Genetic.h"
 
 
+
 Score EvaluateGen(const InputData& data, std::vector<Person> answer){
 
     std::vector<std::vector<int>> result_work_hours;//фактическое количество часов каждый месяц по каждой квалификации
@@ -58,4 +59,59 @@ Score EvaluateGen(const InputData& data, std::vector<Person> answer){
     res.sum_score = res.req_rest_score+res.def_rest_score*10 + res.work_score*1000;
 
     return res;
+}
+
+void RandomGenChange(const InputData& data, std::vector<Person>& gen, const Params& p , int multiplier){
+    std::vector<bool> PriorMonth = data.GetCoolMonth();
+    std::vector<int> MaxFly = data.GetMaxFlies();
+
+    int j =0;
+    for(auto worker: gen){
+        int sum_rest = 0, sum_work = 0;
+        int work_limit = MaxFly[j]*10;
+        for(int i =0; i < p.nMonth; i++){
+            sum_work+=worker.work_hours[i].first;
+            sum_rest+=worker.rest_hours[i];
+        }
+
+
+
+
+
+        j++;
+    }
+
+}
+
+void RunSelection(const InputData& data,
+                  std::vector<Person> start, //задаем началную выборку
+                  int epoch_size, //размер каждого следующего поколения
+                  int survived_part, //какая часть от всех выживает 1/survived_part
+                  bool isIntersection, //делать ли скрещиания лучших
+                  int verbose) //подробность отчётов о работе в консоли от 0 до 5
+{
+    std::cout<<"Running selection"<<std::endl<<std::endl;
+
+
+    Params p;
+    p.REST_YEAR = data.GetRestYear();
+    p.MIN_LAG = data.GetMinLag();
+    p.MIN_SIZE = data.GetMinRestSize();
+    p.TOTAL_RESTS = data.GetTotalRests();
+    p.PRIOR_RESTS = data.GetPriorRests();
+    p.NOPRIOR_RESTS = data.GetNoPriorRests();
+    p.REST_HIGH = data.GetRestHigh();
+    p.REST_LOW = data.GetRestLow();
+    p.nMonth = data.GetNMonth();
+    p.nQ = data.GetNQ();
+
+
+    std::vector<std::pair<std::vector<Person>, Score>> generations;
+    generations.push_back(std::pair<std::vector<Person>, Score>(start, EvaluateGen(data, start)));
+    std::cout<<"Input generation"<<std::endl<<"work_score: "<< generations[0].second.work_score<<std::endl;
+    std::cout<<"def_rest_score: "<< generations[0].second.def_rest_score<<std::endl;
+    std::cout<<"req_rest_score: "<< generations[0].second.req_rest_score<<std::endl;
+    std::cout<<"sum_score: "<< generations[0].second.sum_score<<std::endl<<std::endl;
+
+
 }
--- solution/Genetic.h
+++ solution/Genetic.h
@@ -5,6 +5,7 @@
 #include"input/InputData.h"
 #include<numeric>
 #include<algorithm>
+#include<iostream>
 
 #ifndef RAMAXIMIZATION_RANSHE_BYLO_LUCHSHE_GENETIC_H
 #define RAMAXIMIZATION_RANSHE_BYLO_LUCHSHE_GENETIC_H
@@ -35,12 +36,42 @@ struct Score{
     long long sum_score;
 };
 
+struct Params{
+    int REST_YEAR;
+    int MIN_LAG;
+    int MIN_SIZE;
+    int TOTAL_RESTS;
+    int PRIOR_RESTS ;
+    int NOPRIOR_RESTS ;
+    int REST_HIGH ;
+    int REST_LOW ;
+    int nMonth;
+    int nQ = ;
+};
+
+/*
+ * Изменяем какую то генерацию с указанным показателем multiplier, который определяет
+ * степень изменения того или иного параметра 1/multiplier
+ * Также передаются параметры - p - которые заданы в первом листе входной таблицы
+ */
+void RandomGenChange(const InputData& data, std::vector<Person>& gen, const Params& p, int multiplier = 10);
+
 /*
  * Оцениваем конкретное решение задачи
  * и озвращаем число
  */
 Score EvaluateGen(const InputData& data, std::vector<Person> answer);
 
+/*
+ * Запускаем поиск решения
+ */
+void RunSelection(const InputData& data,
+                  std::vector<Person> start, //задаем началную выборку
+                  int epoch_size = 100, //размер каждого следующего поколения
+                  int survived_part = 10, //какая часть от всех выживает 1/survived_part
+                  bool isIntersection = false, //делать ли скрещиания лучших
+                  int verbose = 5, //подробность отчётов о работе в консоли от 0 до 5
+                  int num_generations_to_stop = 1); //количество генераций до запроса остановки
 
 
 #endif //RAMAXIMIZATION_RANSHE_BYLO_LUCHSHE_GENETIC_H

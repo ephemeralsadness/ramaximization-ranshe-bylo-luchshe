1b6573ad377dc697520c4b74bdaeeeac3be567a1
ephemeralsad <sarancev.dev@gmail.com>
2021-02-21 22:45:22 +1000

    Finished main function

--- solution/constants.h
+++ solution/constants.h
@@ -1,2 +1,25 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
+namespace constants {
+
+    const std::vector<std::string> CSV_TABLE_NAMES = {
+            "max_fly",
+            "max_starts",
+            "months",
+            "params",
+            "personal_levels",
+            "qualified",
+            "qual_levels",
+            "quals",
+            "required_personal",
+            "rest_prior",
+            "rest_req",
+            "starts"
+    };
+
+    const std::string CSV = ".csv";
+
+}

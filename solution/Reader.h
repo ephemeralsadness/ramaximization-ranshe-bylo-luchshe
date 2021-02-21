4e47de963422f31e58d6167fdfe210746c220bd7
ephemeralsad <sarancev.dev@gmail.com>
2021-02-21 22:26:31 +1000

    Finished main function

---
+++ solution/Reader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class Reader {
+public:
+    std::vector<std::vector<std::string>> read_from_csv(const std::string& filename) {
+
+    }
+
+private:
+
+};

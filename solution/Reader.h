33af2d585d6cbc22b61887a854bbcc7fb75ea5a9
ephemeralsad <sarancev.dev@gmail.com>
2021-02-22 00:49:15 +1000

    Changed files

--- solution/Reader.h
+++ solution/Reader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "data/InputData.h"
+
 #include <vector>
 #include <string>
 #include <map>
@@ -8,7 +10,7 @@ class Reader {
 public:
     using StringTable = std::vector<std::vector<std::string>>;
 
-    static void readInputData(const std::string& folderName);
+    static InputData readInputData(const std::string& folderName);
 private:
     static std::map<std::string, StringTable> makeCSVMap(const std::string& folderName);
     static StringTable readFromCSV(const std::string& filename);

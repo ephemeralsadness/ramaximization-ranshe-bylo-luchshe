dbe6714a2ba546e4ca3f64183d37546262d6865a
BLGALEX <lepehaleha@bk.ru>
2021-02-22 18:16:41 +1000

    Заменил русский на английский в выводе

--- solution/Reader.h
+++ solution/Reader.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "input/InputData.h"
-
 #include <vector>
 #include <string>
 #include <map>
--- solution/Writer.cpp
+++ solution/Writer.cpp
@@ -15,21 +15,21 @@ void Writer::writeToCSV(OutputData outputData, std::string outPutFolder) {
     monthResults.open(path+"monthResults.csv");
 
     if(personalRests.is_open()){
-        personalRests<<"Сотрудник,Месяц,Размер отпуска,Заявка\n";
+        personalRests<<"Employee,Month,Rest Size,Request\n";
         for(auto x: outputData.personalRests)
             personalRests<<x.personalId<<','<<x.restSize<<','<<x.month<<','<<x.requestRate<<'\n';
     }
     personalRests.close();
 
     if(totalRests.is_open()){
-        totalRests<<"Сотрудник,Кол-во отпусков,Всего часов,Недобор отпусков\n";
+        totalRests<<"Employee,Rests amount,All hours,Shortage of vacations\n";
         for(auto x: outputData.totalRests)
             totalRests<<x.personalId<<','<<x.nRest<<','<<x.nHour<<','<<x.deficitRest<<'\n';
     }
     totalRests.close();
 
     if(qualDeficits.is_open()){
-        qualDeficits<<"Квалификация,1,2,3,4,5,6,7,8,9,10,11,12\n";
+        qualDeficits<<"Qualification,1,2,3,4,5,6,7,8,9,10,11,12\n";
         for(int i =0; i < 10;i++){
             qualDeficits<<"Q"<<i+1;
             for(int j = 0; j<12;j++){
@@ -42,7 +42,7 @@ void Writer::writeToCSV(OutputData outputData, std::string outPutFolder) {
 
 
     if(workingHours.is_open()){
-        workingHours<<"Квалификация,1,2,3,4,5,6,7,8,9,10,11,12\n";
+        workingHours<<"Qualification,1,2,3,4,5,6,7,8,9,10,11,12\n";
         for(int i =0; i < 10;i++){
             workingHours<<"Q"<<i+1;
             for(int j = 0; j<12;j++){
@@ -54,20 +54,20 @@ void Writer::writeToCSV(OutputData outputData, std::string outPutFolder) {
     workingHours.close();
 
     if(monthResults.is_open()){
-        monthResults<<",1,2,3,4,5,6,7,8,9,10,11,12\nПотребность (часы)";
+        monthResults<<",1,2,3,4,5,6,7,8,9,10,11,12\nNeed (hours)";
 
         for(int i =0; i <12;i++){
         monthResults<<','<<outputData.monthResults[i].needHours;
         }
-        monthResults<<"\nРабочее время (часы)";
+        monthResults<<"\nWorking time (hours)";
         for(int i =0; i <12;i++){
             monthResults<<','<<outputData.monthResults[i].workHours;
         }
-        monthResults<<"\nОтпуска (часы)";
+        monthResults<<"\nHolidays (hours)";
         for(int i =0; i <12;i++){
             monthResults<<','<<outputData.monthResults[i].restInHour;
         }
-        monthResults<<"\nОтпуска (шт)";
+        monthResults<<"\nHolidays (pcs)";
         for(int i =0; i <12;i++){
             monthResults<<','<<outputData.monthResults[i].numberOfRest;
         }

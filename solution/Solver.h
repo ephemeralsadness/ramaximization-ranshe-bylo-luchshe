a230cd513c3717a0c3e2be2b7b0c112ade539f15
BLGALEX <58458024+blgalex@users.noreply.github.com>
2021-02-22 19:35:04 +1000

    Delete Main.ipynb

--- solution/Solver.h
+++ solution/Solver.h
@@ -172,97 +172,104 @@ private:
     }
 
     void fullFill(std::map<int, std::map<Qualification::Id, int>>& possible, OutputData& data) {
+
         auto comp_req_pointers = [](Request lhs, Request rhs) -> bool {
             return lhs.getMonth() < rhs.getMonth();
         };
         auto& cs = constants::REST_CONSTANTS;
 
-        // TODO make MIN_START AND MAX_START work
-        for (auto& pp : inputData.getAllStaff()) {
-            auto id = pp.first;
-            auto human = pp.second;
-            auto& v = staffIdToRequests[id];
+        for (int iter = 0; iter < cs["TOTAL_RESTS"]; ++iter) {
+            for (auto& pp : inputData.getAllStaff()) {
+                auto id = pp.first;
+                auto human = pp.second;
+                auto& v = staffIdToRequests[id];
 
-            if (human.qualifications.empty()) {
-                // (rand() % 12)
-                continue;
-            }
-
-            for (int i = 0; i < 12; ++i) {
-                Request req(id, 3, i, std::min(inputData.getStaff(id).maxHours,
-                        std::max(possible[i][human.qualifications.front()] - inputData.getMonth(i).hoursNeed[human.qualifications.front()], 0)));
-
-                if (req.getHours() == 0) continue;
-
-                if (v.size() >= cs["TOTAL_RESTS"]) continue;
-                if (req.getHours() < cs["MIN_REST_SIZE"]) continue;
-
-                int sm = req.getHours();
-                for (auto r : v)
-                    sm += r.getHours();
-
-                if (sm > cs["REST_YEAR"]) continue;
-
-                {
-                    bool alpha = false;
-                    for (auto r : v) {
-                        alpha |= (abs(r.getMonth() - req.getMonth()) < cs["MIN_REST_LAG"]);
-                    }
-                    if (alpha) continue;
+                if (human.qualifications.empty()) {
+                    // (rand() % 12)
+                    continue;
                 }
 
-                {
-                    int topc = 0;
-                    int notopc = 0;
-                    int toph = 0;
-                    int notoph = 0;
-                    for (auto r : v) {
-                        if (inputData.getMonth(r.getMonth()).isTop) {
-                            ++topc;
-                            toph += r.getHours();
+                for (int i = 0; i < 12; ++i) {
+                    Request req(id, 3, i, std::min(inputData.getStaff(id).maxHours,
+                                                   std::max(possible[i][human.qualifications.front()] - inputData.getMonth(i).hoursNeed[human.qualifications.front()], 0)));
+
+                    if (v.size() != iter)
+                        continue;
+
+                    if (req.getHours() == 0) continue;
+
+                    if (v.size() >= cs["TOTAL_RESTS"]) continue;
+                    if (req.getHours() < cs["MIN_REST_SIZE"]) continue;
+
+                    int sm = req.getHours();
+                    for (auto r : v)
+                        sm += r.getHours();
+
+                    if (sm > cs["REST_YEAR"]) continue;
+
+                    {
+                        bool alpha = false;
+                        for (auto r : v) {
+                            alpha |= (abs(r.getMonth() - req.getMonth()) < cs["MIN_REST_LAG"]);
+                        }
+                        if (alpha) continue;
+                    }
+
+                    {
+                        int topc = 0;
+                        int notopc = 0;
+                        int toph = 0;
+                        int notoph = 0;
+                        for (auto r : v) {
+                            if (inputData.getMonth(r.getMonth()).isTop) {
+                                ++topc;
+                                toph += r.getHours();
+                            } else {
+                                ++notopc;
+                                notoph += r.getHours();
+                            }
+                        }
+
+                        if (inputData.getMonth(req.getMonth()).isTop) {
+                            if (topc == cs["PRIOR_RESTS"] || toph > cs["REST_HIGH"]) {
+                                continue;
+                            }
                         } else {
-                            ++notopc;
-                            notoph += r.getHours();
+                            if (notopc == cs["NOPRIOR_RESTS"] || notoph > cs["REST_LOW"]) {
+                                continue;
+                            }
                         }
                     }
 
-                    if (inputData.getMonth(req.getMonth()).isTop) {
-                        if (topc == cs["PRIOR_RESTS"] || toph > cs["REST_HIGH"]) {
-                            continue;
-                        }
-                    } else {
-                        if (notopc == cs["NOPRIOR_RESTS"] || notoph > cs["REST_LOW"]) {
-                            continue;
+                    {
+                        // check if it will ne deficit. If it will be, than all is bad;
+
+                        Month& m = inputData.getMonth(req.getMonth());
+                        Staff& s = inputData.getStaff(req.getStaffId());
+                        if (!s.qualifications.empty()) {
+                            Qualification& q = inputData.getQualification(s.qualifications.front());
+                            int needHours = req.getHours();
+                            if (possible[req.getMonth()][s.qualifications.front()] - needHours
+                                >= m.hoursNeed[q.name]) {
+                                possible[req.getMonth()][s.qualifications.front()] -= needHours;
+                            } else {
+                                continue;
+                            }
                         }
                     }
+
+                    if (req.getMonth() > 12 || req.getMonth() < 0)
+                        std::cout << req.getMonth();
+
+                    v.push_back(req);
+                    std::sort(v.begin(), v.end(), comp_req_pointers);
+
                 }
-
-                {
-                    // check if it will ne deficit. If it will be, than all is bad;
-
-                    Month& m = inputData.getMonth(req.getMonth());
-                    Staff& s = inputData.getStaff(req.getStaffId());
-                    if (!s.qualifications.empty()) {
-                        Qualification& q = inputData.getQualification(s.qualifications.front());
-                        int needHours = req.getHours();
-                        if (possible[req.getMonth()][s.qualifications.front()] - needHours
-                            >= m.hoursNeed[q.name]) {
-                            possible[req.getMonth()][s.qualifications.front()] -= needHours;
-                        } else {
-                            continue;
-                        }
-                    }
-                }
-
-                if (req.getMonth() > 12 || req.getMonth() < 0)
-                    std::cout << req.getMonth();
-
-                v.push_back(req);
-                std::sort(v.begin(), v.end(), comp_req_pointers);
 
             }
-
         }
+
+
     }
 public:
 
